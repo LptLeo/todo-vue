80ac07f027b109991460dc6c381dacd47bb856e7
Leonardo <lptleo11@gmail.com>
2023-04-12 12:20:03 -0400

    componentizando a aplicação

---
+++ src/App.vue
@@ -0,0 +1,64 @@
+<script setup>
+import { reactive } from 'vue';
+import Cabecalho from './components/cabecalho.vue'
+import Formulario from './components/Formulario.vue'
+import ListaDeTarefas from './components/ListaDeTarefas.vue'
+
+const estado = reactive({
+  filtro: 'todas',
+  tarefaTemp: '',
+  tarefas: [{
+    titulo: 'Estudar ES6',
+    finalizada: false
+  },
+  {
+    titulo: 'Estudar SASS',
+    finalizada: false
+  },
+  {
+    titulo: 'Ir para a academia',
+    finalizada: true,
+  }]
+})
+
+const getTarefasPendentes = () => {
+  return estado.tarefas.filter(tarefa => !tarefa.finalizada)
+}
+
+const getTarefasFinalizadas = () => {
+  return estado.tarefas.filter(tarefa => tarefa.finalizada)
+}
+
+const getTarefasFiltradas = () => {
+  const { filtro } = estado;
+
+  switch (filtro) {
+    case 'pendentes':
+      console.log(getTarefasPendentes())
+      return getTarefasPendentes()
+    case 'finalizadas':
+      console.log(getTarefasFinalizadas())
+      return getTarefasFinalizadas()
+    default:
+      console.log(estado.tarefas)
+      return estado.tarefas
+  }
+}
+
+const cadastraTarefa = () => {
+  const tarefaNova = {
+    titulo: estado.tarefaTemp,
+    finalizada: false
+  }
+  estado.tarefas.push(tarefaNova)
+  estado.tarefaTemp = ''
+}
+</script>
+
+<template>
+  <div class="container">
+    <Cabecalho :tarefas-pendentes="getTarefasPendentes().length"></Cabecalho>
+    <Formulario :trocar-filtro="evento => estado.filtro = evento.target.value" :tarefa-temp="estado.tarefaTemp" :edita-tarefa-temp="evento => estado.tarefaTemp = evento.target.value" :cadastra-tarefa="cadastraTarefa"></Formulario>
+    <ListaDeTarefas :tarefas="getTarefasFiltradas()"></ListaDeTarefas>
+  </div>
+</template>
